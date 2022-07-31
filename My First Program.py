33471aaf6979d5b139df1e79a5c6712681f985e9
Prathamesh <110330280+Prathamesh0401@users.noreply.github.com>
2022-07-31 15:56:38 +0530

    Add files via upload

---
+++ My First Program.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import pandas as pd
+
+
+# In[14]:
+
+
+pd.read_csv(r'C:\Users\Prathmesh Lanjekar\Desktop\My Program 1.csv')
+
+
+# In[16]:
+
+
+pd.read_excel(r'C:\Users\Prathmesh Lanjekar\Desktop\My Program.xlsx')
+
+
+# In[19]:
+
+
+pd.read_csv(r'C:\Users\Prathmesh Lanjekar\Desktop\tsv.txt')
+
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+p=9
+g=34
+q=(p+g)
+print(q)
+
+
+# In[4]:
+
+
+pd.read_excel(r'C:\Users\Prathmesh Lanjekar\Desktop\My Program.xlsx')
+
+
+# In[6]:
+
+
+pd.read_csv(r'C:\Users\Prathmesh Lanjekar\Desktop\My Program 1.csv')
+
+
+# In[8]:
+
+
+pd.read_csv(r'C:\Users\Prathmesh Lanjekar\Desktop\tsv.txt')
+
+
+# In[10]:
+
+
+pd.read_csv(r'C:\Users\Prathmesh Lanjekar\Desktop\student.tsv')
+
+
+# In[11]:
+
+
+pd.read_csv('https://raw.githubusercontent.com/shivam229/dsrscientist-DSData/master/Advertising.csv')
+
+
+# In[12]:
+
+
+pd.read_csv('https://raw.githubusercontent.com/shivam229/dsrscientist-DSData/master/Concrete_Data.csv')
+
+
+# In[ ]:
+
+
+
+
